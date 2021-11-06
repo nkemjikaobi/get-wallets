e5eb1784de3a9238e064cc64992cf344a8f779a2
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-11-06 01:14:44 +0100

    CleanUp files, add eslint, prettier, jsconfig files

---
+++ src/Configurations/configurations.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const config: any = {
+	web: {
+		baseUrl: process.env.REACT_APP_BASE_URL,
+		secretKey: process.env.REACT_APP_SECRET_KEY,
+	},
+};
+
+export default config;
