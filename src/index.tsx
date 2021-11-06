46253224d9a18c0d3249129ebc43575c3b6b1736
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-11-06 01:49:49 +0100

    Added routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes/routes";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
